6a8c611a6b87caaf5ddfdeeedc895ab80f9ba5c9
Jager-yoo <y73447jh@gmail.com>
2021-10-29 01:55:37 +0900

    feat: Alert 발동 버튼 추가
    - Alert 모달 생성 메서드 정의 코드를 실제로 Alert를 호출할 WhiteViewController로 옮김

--- playground/ViewController.swift
+++ playground/ViewController.swift
@@ -14,23 +14,5 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    private func showAlert() {
-        let message = "Alert 내용"
-        
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        
-        // style 로 선택할 수 있는 3가지 열거형을 모두 만들어보자!
-        let defaultAction = UIAlertAction(title: "Default", style: .default)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        let destructiveAction = UIAlertAction(title: "Destruct", style: .destructive)
-        
-        // default, cancel, destructive 순서대로 alert 에 액션을 넣어준다.
-        alert.addAction(defaultAction)
-        alert.addAction(cancelAction)
-        alert.addAction(destructiveAction)
-        
-        // alert 모달을 화면에 띄우는 메서드
-        present(alert, animated: true)
-    }
 }
 
--- playground/WhiteViewController.swift
+++ playground/WhiteViewController.swift
@@ -15,15 +15,22 @@ class WhiteViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showAlert() {
+        let message = "Alert 내용"
+        
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        
+        // style 로 선택할 수 있는 3가지 열거형을 모두 만들어보자!
+        let defaultAction = UIAlertAction(title: "Default", style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let destructiveAction = UIAlertAction(title: "Destruct", style: .destructive)
+        
+        // default, cancel, destructive 순서대로 alert 에 액션을 넣어준다.
+        alert.addAction(defaultAction)
+        alert.addAction(cancelAction)
+        alert.addAction(destructiveAction)
+        
+        // alert 모달을 화면에 띄우는 메서드
+        present(alert, animated: true)
     }
-    */
-
 }
