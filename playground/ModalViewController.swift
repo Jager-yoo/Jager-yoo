43130b0dc08a4e1245c0f45866ed50224d216ea4
Jager-yoo <y73447jh@gmail.com>
2021-10-30 17:22:45 +0900

    feat: Modal 실험용 버튼 2개 추가하고 다음 View 와 연결

---
+++ playground/ModalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ModalViewController.swift
+//  playground
+//
+//  Created by 유재호 on 2021/10/30.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
